a6d8d28fc57061d5c259c239d570de440bfd16b1
liuhuiliu <765226421@qq.com>
2021-07-26 15:19:33 +0800

    deploy

---
+++ precache-manifest.dfa6be9ee66671c354cd3e061559addc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5576ee26d4d576a6280",
+    "url": "/project-vue-1-show/css/app.b97cea5a.css"
+  },
+  {
+    "revision": "1a744e6b1fa876f503023b991e8a5c86",
+    "url": "/project-vue-1-show/index.html"
+  },
+  {
+    "revision": "d5576ee26d4d576a6280",
+    "url": "/project-vue-1-show/js/app.441ce9ac.js"
+  },
+  {
+    "revision": "5c80f85038b3880fb371",
+    "url": "/project-vue-1-show/js/chunk-vendors.20fceb74.js"
+  },
+  {
+    "revision": "fda9b8cbb784a11d164e38c525da76ab",
+    "url": "/project-vue-1-show/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/project-vue-1-show/robots.txt"
+  }
+]);
